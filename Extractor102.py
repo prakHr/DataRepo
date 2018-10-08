bb2c0078683b63fb5cb7853488b63ef3b1030f0a
zorro-p <f20150456@hyderabad.bits-pilani.ac.in>
2018-10-08 14:23:58 +0530

    Removed checks and flags

--- Extractor102.py
+++ Extractor102.py
@@ -1,10 +1,8 @@
+#!/usr/bin/env python3
 import firebase_admin
 from firebase_admin import *
 from firebase_admin import firestore
 import json
-#!/usr/bin/env python3
-
-
 
 #Credentials of project1 and project2 is required
 cred1=credentials.Certificate("python-78039-firebase-adminsdk-mthis-66f13748f8.json")
@@ -26,20 +24,31 @@ flags=[]
 for doc in docs:
     dictionaryOfDoc=doc.to_dict()
     #print(dictionaryOfDoc['check'])
-    flags.append(dictionaryOfDoc['check'])
+    #flags.append(dictionaryOfDoc['check'])
     db1.collection(u'Kiranas').document(doc.id).set(doc.to_dict())
     ArrayToBeKeepedIn.append(doc.id)
 #print(ArrayToBeKeepedIn)
+#Extracts documents from barcodesCollection and set in another project according to check
+flagLenDash,num=0,0
+for i in (ArrayToBeKeepedIn):
+    num+=1
+    #if flags[flagLenDash]=='false' or flags[flagLenDash]=='False' or flags[flagLenDash]==0 or flags[flagLenDash]=='0':
+      #  flagLenDash+=1
+       # continue
+    #flagLenDash+=1
+    BarcodesCollections=db.collection(u'Kiranas').document(i).collection(u'Barcodes').get()
+    for docu in BarcodesCollections:
+        db1.collection(u'Kiranas').document(i).collection(u'Barcodes').document(docu.id).set(docu.to_dict())
+
 SecondArray,flagLen,num=[],0,0
 for i in ArrayToBeKeepedIn:#iterates through the array
-    #if i =='Navkar_store':#Since Navkar_store has speechCollection
-    if flags[flagLen]=='false' or flags[flagLen]=='False' or flags[flagLen]==0 or flags[flagLen]=='0':
-        flagLen+=1
-        continue
-    flagLen+=1
+    #if flags[flagLen]=='false' or flags[flagLen]=='False' or flags[flagLen]==0 or flags[flagLen]=='0':
+      #  flagLen+=1
+       # continue
+    #flagLen+=1
     num+=1
     SpeechCollections=db.collection(u'Kiranas').document(i).collection(u'SpeechItems').get()#takes all the documents(Id and data) from SpeechItems collection
-    
+
     for docu in SpeechCollections:
         #print(u'{} => {}'.format(docu.id, docu.to_dict()))
         db1.collection(u'Kiranas').document(i).collection(u'SpeechItems').document(docu.id).set(docu.to_dict())#reverse engineering to set all the data in SpeechItems according to document Id
@@ -55,13 +64,12 @@ for i in ArrayToBeKeepedIn:#iterates through the array
             for stockDocuments in stockcollection:
                 #print(u'{} => {}'.format(stockDocuments.id, stockDocuments.to_dict()))
                 db1.collection(u'Kiranas').document(i).collection(u'SpeechItems').document(docu.id).collection(u'speechInventoryPrice').document(docu2.id).collection(u'stockcollection').document(stockDocuments.id).set(stockDocuments.to_dict())
-
             #collection Kirana=>document(Navkar)=>collection speechItems=>random documents=>collection speechInventoryPrice=>random documents=>pricecollection,stockcollection
-           
-            
+
+
 num=0
 
 
-            
 
-    
+
+
