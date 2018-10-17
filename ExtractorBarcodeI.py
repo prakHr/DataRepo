2bb29c8de30ca9d05df5699bedb618ba9e94eb54
zorro-p <f20150456@hyderabad.bits-pilani.ac.in>
2018-10-17 14:58:21 +0530

    added comments

--- ExtractorBarcodeI.py
+++ ExtractorBarcodeI.py
@@ -17,23 +17,27 @@ db1=extractDatabase(cred1)
 barcode_inventory_docs=db1.collection(u'barcode_inventory').get()
 barcode_inventory_ref=db1.collection(u'barcode_inventory')
 
+#for loop takes distinct barcodes using lower method of string in a set
 barcodes_set=set()
 for doc in barcode_inventory_docs:
     #print(doc.id)
     b=doc.id
     barcodes_set.add(b.lower())
 
+#for loop takes all the possible lengths of barcodes in a set(surprised this set could have any possible permutation but it gave increasing order of lengths)
 barcodes_len_set=set()
 for b in barcodes_set:
     barcodes_len_set.add(len(b))
 #print(barcodes_len_set)#{3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20}
 
+#for loop counts the number of barcodes using its length
 for l in barcodes_len_set:
     count=0
     for b in barcodes_set:
         if len(b)==l:
             count+=1
     #print(l,count)
+    
     #3 1
     #4 1
     #5 1
@@ -50,6 +54,7 @@ for l in barcodes_len_set:
     #16 2
     #20 1
 
+#for loops keeps barcodes with first and last same digits into an arraySet=>(DoubleDigit,len(corresponding_barcode))
 sameLastTwoDigitArraySet,sameFirstTwoDigitArraySet=set(),set()
 for b in barcodes_set:
     if b[0]==b[1]:
@@ -58,9 +63,14 @@ for b in barcodes_set:
         sameLastTwoDigitArraySet.add((b[-1]*2,len(b)))
 #print('sameLastTwoDigitArraySet=>',sameLastTwoDigitArraySet)
 #{('00', 12), ('11', 13), ('hh', 3), ('00', 8), ('00', 13), ('33', 8), ('00', 9), ('99', 12), ('33', 12), ('88', 12), ('55', 13), ('11', 11), ('77', 13), ('66', 13), ('22', 8), ('99', 13), ('44', 8), ('33', 13), ('88', 13), ('22', 12), ('44', 12), ('55', 12), ('11', 8), ('99', 10), ('77', 12), ('55', 8), ('11', 12), ('33', 10), ('77', 8), ('22', 13), ('00', 11), ('44', 13), ('33', 14), ('88', 14)}
+#print('count of same last 2 digit is :-'+,len(sameLastTwoDigitArraySet))
 sameFirstTwoDigit=set()
 #print('sameFirstTwoDigitArraySet=>',sameFirstTwoDigitArraySet)
 #{('66', 11), ('88', 10), ('88', 14), ('77', 12), ('00', 12), ('99', 12), ('44', 14), ('88', 13), ('99', 13), ('88', 12), ('11', 4), ('77', 13), ('66', 14)}
+#print('count of same first 2 digit is :-'+,len(sameFirstTwoDigitArraySet))
+
+#function takes database(optional args, not necessary) and reference of main collection as input and then gives an array of [[barcodeNumber1,lower(barcodeName1),barcodePrice1],...]
+#other outputs are :-a price dictionary of key-value pair as {(barcodeNumber1,lower(barcodePrice1)),(barcodeNumber2,lower(barcodePrice2))..}, a name dictionary of key-value pair as {(barcodeNumber1,lower(barcodeName1)),(barcodeNumber2,lower(barcodeName2))...}
 def extractNameAndPriceFromBarcodes(database,reference):
     array=[]
     price_dict,name_dict={},{}
@@ -79,33 +89,10 @@ barcodesArray,barcodesPriceDict,barcodesNameDict=extractNameAndPriceFromBarcodes
 #print('barcodesPriceDict=>',barcodesPriceDict)
 #print('barcodesNameDict=>',barcodesNameDict)
 
-def TwoSameNames(arraySet):
-    s=set()
-    for (a,b) in arraySet:
-        s.add(a)
-    return s
-
-sameFirstTwoDigit,sameLastTwoDigit=TwoSameNames(sameFirstTwoDigitArraySet),TwoSameNames(sameLastTwoDigitArraySet)
-
-def distinctDoubleDigits(arrayContainingSameDigits,arraySet):
-    g=0
-    DOUBLE_DIGITS=[]
-    for double_digits in arrayContainingSameDigits:
-        previous_g=0
-        for (a,b) in arraySet:
-            if double_digits==a:
-                g=max(b,g)
-        DOUBLE_DIGITS.append([double_digits,g])
-        g=0
-    return DOUBLE_DIGITS
-
-SameDoubleDigits1=distinctDoubleDigits(sameFirstTwoDigit,sameFirstTwoDigitArraySet)
-#print(SameDoubleDigits1)
-#[['88', 14], ['00', 12], ['66', 14], ['99', 13], ['11', 4], ['77', 13]]
-SameDoubleDigits2=distinctDoubleDigits(sameLastTwoDigit,sameLastTwoDigitArraySet)
-#print(SameDoubleDigits2)
-#[['44', 13], ['22', 13], ['hh', 3], ['77', 13], ['33', 14], ['99', 13], ['88', 14], ['55', 13], ['11', 13], ['66', 13], ['00', 13]]
-
+#function takes digits,(digits+1),barcodesSet,price dictionary,name dictionary(where digits come from an array in the for loop) and output is 2 sets that takes barcodes of corresponding lengths(digits,digits+1) and becomes an input for subfunction
+#function also contains a sub function that takes 2 sets and a dictionary as input and gives output in array of the form [count,Dict[key1],a,b] iff a=b where a=Dict[digits],b=Dict[digits+1]
+#so according to nature of dictionaries(name/Price) Dict[digits]=name/Price which gives similarity
+#output is gonna be 2 arrays that contains barcodes in the format [count,sameValue(either price/name),barcode1(of len digits),barcode2(of len digits-1)]
 def similarity(bLen_minus_one_digit,bLen_digit,barcodes,priceDict,nameDict):
     bLen_minus_one_digit_set,bLen_digit_set=set(),set()
     for b in barcodes:
@@ -129,28 +116,6 @@ def similarity(bLen_minus_one_digit,bLen_digit,barcodes,priceDict,nameDict):
     
     set1,set2=bLen_minus_one_digit_set,bLen_digit_set
     CountArrayForPrices,CountArrayForNames=function(set1,set2,priceDict),function(set1,set2,nameDict)
-    '''
-    CountArrayForPrices,CountArrayForNames=[],[]
-    for b_minus_one in bLen_minus_one_digit_set:
-        count=1
-        if b_minus_one not in priceDict:continue
-        for b_one in bLen_digit_set:#need to add visited as a data structure as well
-            if b_one not in priceDict:continue
-            if b_minus_one not in priceDict:continue
-            if priceDict[b_minus_one] ==priceDict[b_one]:
-                count+=1
-        CountArrayForPrices.append([count,priceDict[b_minus_one],b_minus_one,b_one])
-
-    for b_minus_one in bLen_minus_one_digit_set:
-        count=1
-        if b_minus_one not in nameDict:continue
-        for b_one in bLen_digit_set:
-            if b_one not in nameDict:continue
-            if b_minus_one not in nameDict:continue
-            if nameDict[b_minus_one] ==nameDict[b_one]:
-                count+=1
-        CountArrayForNames.append([count,nameDict[b_minus_one],b_minus_one,b_one])
-        '''
     return CountArrayForPrices,CountArrayForNames
     
 x_minus_one_digit=[3,4,5,6,7,8,9,10,11,12,13,14,15]
