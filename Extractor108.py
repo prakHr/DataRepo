2d153295ec33db9f7aee427b82d06e743a3c368e
zorro-p <f20150456@hyderabad.bits-pilani.ac.in>
2018-10-18 11:45:11 +0530

    corrected syntax

--- Extractor108.py
+++ Extractor108.py
@@ -7,8 +7,7 @@ from datetime import timedelta
 
 _AUTO_ID_CHARS = ('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
 
-def _auto_id():
-    return ''.join(random.choice(_AUTO_ID_CHARS) for _ in six.moves.xrange(20))
+def _auto_id():return ''.join(random.choice(_AUTO_ID_CHARS) for _ in six.moves.xrange(20))
 
 #project-python(is filled with munshik3 data) and project2(is fiilled with projectMehboob)
 cred1=credentials.Certificate("python-78039-firebase-adminsdk-mthis-66f13748f8.json")
@@ -42,7 +41,8 @@ def extractionFromBarcodesCollection(b_collection,path):
         
         ID,my_dict=random_doc.id,random_doc.to_dict()
         
-        if 'name' not in my_dict:flag1=True
+        if 'name' not in my_dict:
+            flag1=True
         if 'barcode' not in my_dict:flag2=True
         if 'price' not in my_dict:flag3=True
         if 'modified' not in my_dict:flag4=True
@@ -50,32 +50,33 @@ def extractionFromBarcodesCollection(b_collection,path):
         pathDash=path+'/'+ID+'/'
         
         if flag1==True:#here we need to check all conditions using if-statements
-            nowTime=time.time();nowTime=math.ceil(nowTime*1000)
+            nowTime=time.time()
+            nowTime=math.ceil(nowTime*1000)
             ts=str(nowTime)
             reasons.append(['Name',pathDash,ts])
         if flag2==True:nowTime=time.time();nowTime=math.ceil(nowTime*1000);ts=str(nowTime);reasons.append(['Number',pathDash,ts])
         if flag3==True:nowTime=time.time();nowTime=math.ceil(nowTime*1000);ts=str(nowTime);reasons.append(['Price',pathDash,ts])
         if flag4==True:nowTime=time.time();nowTime=math.ceil(nowTime*1000);ts=str(nowTime);reasons.append(['Modified',pathDash,ts])
 
-            
-        if flag1==True and flag2==True and flag3==True and flag4==True:#gonna be 2<<3 cases i.e. 16 cases
+        
+        if (flag1==True and flag2==True and flag3==True and flag4==True):#gonna be 2<<3 cases i.e. 16 cases
             name,number,price,modified='','',[0],''
-        elif flag1==True and flag2==True and flag3==False and flag4==True:name,number,price,modified='','',my_dict['price'],''
-        elif flag1==True and flag2==False and flag3==True and flag4==True:name,number,price,modified='',my_dict['barcode'],[0],''
-        elif flag1==False and flag2==True and flag3==True and flag4==True:name,number,price,modified=my_dict['name'],'',[0],''
-        elif flag1==True and flag2==False and flag3==False and flag4==True:name,number,price,modified='',my_dict['barcode'],my_dict['price'],''
-        elif flag1==False and flag2==False and flag3==True and flag4==True:name,number,price,modified=my_dict['name'],my_dict['barcode'],[0],''
-        elif flag1==False and flag2==False and flag3==False and flag4==True:name,number,price,modified=my_dict['name'],my_dict['barcode'],my_dict['price'],''
-        elif flag1==False and flag2==True and flag3==False and flag4==True:name,number,price,modified=my_dict['name'],'',my_dict['price'],''
-        elif flag1==True and flag2==True and flag3==True and flag4==False:#gonna check all cases if we use if-statements instead of elif
+        elif (flag1==True and flag2==True and flag3==False and flag4==True):name,number,price,modified='','',my_dict['price'],''
+        elif (flag1==True and flag2==False and flag3==True and flag4==True):name,number,price,modified='',my_dict['barcode'],[0],''
+        elif (flag1==False and flag2==True and flag3==True and flag4==True):name,number,price,modified=my_dict['name'],'',[0],''
+        elif (flag1==True and flag2==False and flag3==False and flag4==True):name,number,price,modified='',my_dict['barcode'],my_dict['price'],''
+        elif (flag1==False and flag2==False and flag3==True and flag4==True):name,number,price,modified=my_dict['name'],my_dict['barcode'],[0],''
+        elif (flag1==False and flag2==False and flag3==False and flag4==True):name,number,price,modified=my_dict['name'],my_dict['barcode'],my_dict['price'],''
+        elif (flag1==False and flag2==True and flag3==False and flag4==True):name,number,price,modified=my_dict['name'],'',my_dict['price'],''
+        elif (flag1==True and flag2==True and flag3==True and flag4==False):#gonna check all cases if we use if-statements instead of elif
             name,number,price,modified='','',[0],my_dict['modified']
-        elif flag1==True and flag2==True and flag3==False and flag4==False:name,number,price,modified='','',my_dict['price'],my_dict['modified']
-        elif flag1==True and flag2==False and flag3==True and flag4==False:name,number,price,modified='',my_dict['barcode'],[0],my_dict['modified']
-        elif flag1==False and flag2==True and flag3==True and flag4==False:name,number,price,modified=my_dict['name'],'',[0],my_dict['modified']
-        elif flag1==True and flag2==False and flag3==False and flag4==False:name,number,price,modified='',my_dict['barcode'],my_dict['price'],my_dict['modified']
-        elif flag1==False and flag2==False and flag3==True and flag4==False:name,number,price,modified=my_dict['name'],my_dict['barcode'],[0],my_dict['modified']
-        elif flag1==False and flag2==False and flag3==False and flag4==False:name,number,price,modified=my_dict['name'],my_dict['barcode'],my_dict['price'],my_dict['modified']
-        elif flag1==False and flag2==True and flag3==False and flag4==False:name,number,price,modified=my_dict['name'],'',my_dict['price'],my_dict['modified']
+        elif (flag1==True and flag2==True and flag3==False and flag4==False):name,number,price,modified='','',my_dict['price'],my_dict['modified']
+        elif (flag1==True and flag2==False and flag3==True and flag4==False):name,number,price,modified='',my_dict['barcode'],[0],my_dict['modified']
+        elif (flag1==False and flag2==True and flag3==True and flag4==False):name,number,price,modified=my_dict['name'],'',[0],my_dict['modified']
+        elif (flag1==True and flag2==False and flag3==False and flag4==False):name,number,price,modified='',my_dict['barcode'],my_dict['price'],my_dict['modified']
+        elif (flag1==False and flag2==False and flag3==True and flag4==False):name,number,price,modified=my_dict['name'],my_dict['barcode'],[0],my_dict['modified']
+        elif (flag1==False and flag2==False and flag3==False and flag4==False):name,number,price,modified=my_dict['name'],my_dict['barcode'],my_dict['price'],my_dict['modified']
+        elif (flag1==False and flag2==True and flag3==False and flag4==False):name,number,price,modified=my_dict['name'],'',my_dict['price'],my_dict['modified']
             
         array.append([modified,name,number,price,len(price)])#array[0]=modified,array[1]=name,array[2]=number,array[3]=price,array[4]=len(price)
     return array,reasons
@@ -93,7 +94,9 @@ for doc in DocumentsArray:
     if 'transferTo' in my_dict:
         
         idToTransferAtdb1=my_dict['transferTo']
-        if idToTransferAtdb1[0:3]!='+91':#if transferTo doesn't contain +91,then
+        
+        #if transferTo doesn't contain +91,then
+        if idToTransferAtdb1[0:3]!='+91':
             idToTransferAtdb1='+91'+my_dict['transferTo']
         
         BarcodesCollection=db2.collection(u'Kiranas').document(ids).collection(u'Barcodes').get()    
@@ -114,14 +117,68 @@ for doc in DocumentsArray:
             modifiedValue=everythingArray[i][0]
             if modifiedValue==False:
                 priceArrayLengthIndb2=everythingArray[i][4]
-                if priceArrayLengthIndb2==1:db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).set({'barcodeName':everythingArray[i][1],'barcodeNumber':everythingArray[i][2],'barcodePrice':everythingArray[i][3][0]})
-                elif priceArrayLengthIndb2==2:db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).set({'barcodeName':everythingArray[i][1],'barcodeNumber':everythingArray[i][2],'barcodePrice':everythingArray[i][3][0],'barcodePrice2':everythingArray[i][3][1]})
-                elif priceArrayLengthIndb2==3:db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).set({'barcodeName':everythingArray[i][1],'barcodeNumber':everythingArray[i][2],'barcodePrice':everythingArray[i][3][0],'barcodePrice2':everythingArray[i][3][1],'barcodePricePkt':everythingArray[i][3][2]})
+                if (priceArrayLengthIndb2==1):
+                    db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).set
+                    (
+                        {
+                            'barcodeName':everythingArray[i][1],
+                            'barcodeNumber':everythingArray[i][2],
+                            'barcodePrice':everythingArray[i][3][0]
+                        }
+                     )
+                elif (priceArrayLengthIndb2==2):
+                    db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).set
+                    (
+                        {
+                            'barcodeName':everythingArray[i][1],
+                            'barcodeNumber':everythingArray[i][2],
+                            'barcodePrice':everythingArray[i][3][0],
+                            'barcodePrice2':everythingArray[i][3][1]
+                        }
+                    )
+                elif (priceArrayLengthIndb2==3):
+                    db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).set
+                    (
+                        {
+                            'barcodeName':everythingArray[i][1],
+                            'barcodeNumber':everythingArray[i][2],
+                            'barcodePrice':everythingArray[i][3][0],
+                            'barcodePrice2':everythingArray[i][3][1],
+                            'barcodePricePkt':everythingArray[i][3][2]
+                        }
+                    )
             elif modifiedValue==True:
                 priceArrayLengthIndb2=everythingArray[i][4]
-                if priceArrayLengthIndb2==1:db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).update({'barcodeName':everythingArray[i][1],'barcodeNumber':everythingArray[i][2],'barcodePrice':everythingArray[i][3][0]})
-                elif priceArrayLengthIndb2==2:db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).update({'barcodeName':everythingArray[i][1],'barcodeNumber':everythingArray[i][2],'barcodePrice':everythingArray[i][3][0],'barcodePrice2':everythingArray[i][3][1]})
-                elif priceArrayLengthIndb2==3:db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).update({'barcodeName':everythingArray[i][1],'barcodeNumber':everythingArray[i][2],'barcodePrice':everythingArray[i][3][0],'barcodePrice2':everythingArray[i][3][1],'barcodePricePkt':everythingArray[i][3][2]})
+                if (priceArrayLengthIndb2==1):
+                    db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).update
+                    (
+                        {
+                            'barcodeName':everythingArray[i][1],
+                            'barcodeNumber':everythingArray[i][2],
+                            'barcodePrice':everythingArray[i][3][0]
+                        }
+                    )
+                elif (priceArrayLengthIndb2==2):
+                    db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).update
+                    (
+                        {
+                            'barcodeName':everythingArray[i][1],
+                            'barcodeNumber':everythingArray[i][2],
+                            'barcodePrice':everythingArray[i][3][0],
+                            'barcodePrice2':everythingArray[i][3][1]
+                        }
+                     )
+                elif (priceArrayLengthIndb2==3):
+                    db1.collection(u'users').document(idToTransferAtdb1).collection(u'barcode_inventory').document(everythingArray[i][2]).update
+                    (
+                        {
+                            'barcodeName':everythingArray[i][1],
+                            'barcodeNumber':everythingArray[i][2],
+                            'barcodePrice':everythingArray[i][3][0],
+                            'barcodePrice2':everythingArray[i][3][1],
+                            'barcodePricePkt':everythingArray[i][3][2]
+                        }
+                     )
             
         speechArray=Extractor(SpeechItemsCollectionRef)
         for docs in speechArray:
@@ -130,9 +187,16 @@ for doc in DocumentsArray:
             
     elif 'transferTo' not in my_dict:
         nowTime=time.time();nowTime=math.ceil(nowTime*1000);ts=str(nowTime)
-        reason1=str('transferTo field not found')
+        reason1='transferTo field not found'
         pathVariable='/Kiranas/'+ids
-        db2.collection(mainCollectionCreated).document(_auto_id()).set({'path':pathVariable,'reason':reason1,'timestamp':ts,'transferRequestFor':str(ids)})#autogenerateID(need to print path to find its type in python)
+        db2.collection(mainCollectionCreated).document(_auto_id()).set
+        (
+            {'path':pathVariable,
+             'reason':reason1,
+             'timestamp':ts,
+             'transferRequestFor':str(ids)
+             }
+        )#autogenerateID(need to print path to find its type in python)
 
 
 
