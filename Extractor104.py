a6aeb97d786163cda5421ce3f2ef0ed9285daaba
zorro-p <f20150456@hyderabad.bits-pilani.ac.in>
2018-10-23 12:56:09 +0530

    minimized ugly looking sphagetti code a little bit

--- Extractor104.py
+++ Extractor104.py
@@ -1,24 +1,17 @@
-#took 1276 secs=>convert into time 21.266
 import firebase_admin
 from firebase_admin import *
 from firebase_admin import firestore
 import time
-from datetime import timedelta
-
-start_time=time.time()
-cred1=credentials.Certificate("python-78039-firebase-adminsdk-mthis-66f13748f8.json")
+import sys
 cred2=credentials.Certificate("project1-4e7b7-firebase-adminsdk-7irgt-433da006ab.json")
-
+# database extraction using the credential
 def extractDatabase(cred):
     app=firebase_admin.initialize_app(cred)
     db=firestore.client()
     delete_app(app)
     return db
-
-db= extractDatabase(cred1)
 db1=extractDatabase(cred2)
-
-#all the names of known collection stored in variables
+# variables to store the collection names
 collection1,collection2,collection3,collection4,collection5,collection6,collection7,collection8,collection9=u'barcode_inventory',u'barcode_repeats',u'bills',u'customers',u'speech_inventory',u'tags',u'unlisted_barcode_inventory',u'users',u'barcode_repeats'
 usersCollection1,usersCollection2,usersCollection3,usersCollection4=collection1,collection3,collection4,collection5
 billsCollection1=u'sold'
@@ -27,88 +20,54 @@ collectionOfUsersCollection2=u'sold'
 collectionsOfUsersCollection4=u'speechInventoryPrice'
 collectionOfSpeechInventoryPrice=u'records'
 collectionOfRecords=[u'price',u'stock']
-ref1,ref2,ref3,ref4,ref5,ref6,ref7,ref8,ref9=db.collection(collection1),db.collection(collection2),db.collection(collection3),db.collection(collection4),db.collection(collection5),db.collection(collection6),db.collection(collection7),db.collection(collection8),db.collection(collection9)
-
-#function takes pair of (random documentsID,key-value pair from Add field) from main collections that don't contain subcollections
-#Output=>sets the values of main collection in another project
-def SimplePutIntoCollection(collection,reference,database):
-    docs=reference.get()
-    for doc in docs:
-        database.collection(collection).document(doc.id).set(doc.to_dict())
-
-#function stores the id of random documents present in main-collection's into array
-def storesDocumentIdIntoArrayAndSetsDatabase(collection,reference,database):
-    docs,array=reference.get(),[]
-    for doc in docs:
-        array.append(doc.id)
-        database.collection(collection).document(doc.id).set(doc.to_dict())
-    return array
-
-SimplePutIntoCollection(collection9,ref9,db1)
-SimplePutIntoCollection(collection1,ref1,db1),SimplePutIntoCollection(collection2,ref2,db1),SimplePutIntoCollection(collection4,ref4,db1),SimplePutIntoCollection(collection5,ref5,db1),SimplePutIntoCollection(collection6,ref6,db1),SimplePutIntoCollection(collection7,ref7,db1)
-
-#with the help of arrays, and the known names of subcollections repeated the process for it like main-collection's but this time we need not put into array
-billsArray=storesDocumentIdIntoArrayAndSetsDatabase(collection3,ref3,db1)
-for randomID in billsArray:
-    DocumentsOfBillsCollection=db.collection(collection3).document(randomID).collection(billsCollection1).get()
-    for doc in DocumentsOfBillsCollection:
-        db1.collection(collection3).document(randomID).collection(billsCollection1).document(doc.id).set(doc.to_dict())
-    
-usersArray=storesDocumentIdIntoArrayAndSetsDatabase(collection8,ref8,db1)
-
-for randomID in usersArray:
-    DocumentsOfBarcodeInventoryCollection=db.collection(collection8).document(randomID).collection(usersCollection1).get()
+ref1,ref2,ref3,ref4,ref5,ref6,ref7,ref8,ref9=db1.collection(collection1),db1.collection(collection2),db1.collection(collection3),db1.collection(collection4),db1.collection(collection5),db1.collection(collection6),db1.collection(collection7),db1.collection(collection8),db1.collection(collection9)
+# variables to decide what all collections we have to transfer
+transferBarcode=False
+transferSpeech=True
+transferBill=False
+transferCustomer=False
+# from ID and to ID - retrieved from the command
+# first argument is the from ID and the second argument is the to ID
+toID = sys.argv[2]#run it as python scriptName.py arg1 arg2,where argv[0]=scriptName,argv[1]=arg1,argv[2]=arg2
+fromID = sys.argv[1]
+# transfer the barcode collection data if transferBarcode value is true
+if(transferBarcode):
+    DocumentsOfBarcodeInventoryCollection=db1.collection(collection8).document(fromID).collection(usersCollection1).get()
+# DocumentsOfBarcodeInventoryCollection=db1.collection(usersCollection1).get()
     for doc in DocumentsOfBarcodeInventoryCollection:
-        db1.collection(collection8).document(randomID).collection(usersCollection1).document(doc.id).set(doc.to_dict())
-    
-    DocumentsOfBillsCollection=db.collection(collection8).document(randomID).collection(usersCollection2).get()
+        db1.collection(collection8).document(toID).collection(usersCollection1).document(doc.id).set(doc.to_dict())
+# transfer the bill collection data if transferBill value is true
+if(transferBill):
+    DocumentsOfBillsCollection=db1.collection(collection8).document(fromID).collection(usersCollection2).get()
     for doc in DocumentsOfBillsCollection:
-        db1.collection(collection8).document(randomID).collection(usersCollection2).document(doc.id).set(doc.to_dict())
-        
-        DocumentsOfSoldCollection=db.collection(collection8).document(randomID).collection(usersCollection2).document(doc.id).collection(collectionOfUsersCollection2).get()
+        db1.collection(collection8).document(toID).collection(usersCollection2).document(doc.id).set(doc.to_dict())
+        DocumentsOfSoldCollection=db1.collection(collection8).document(fromID).collection(usersCollection2).document(doc.id).collection(collectionOfUsersCollection2)
         for docu11 in DocumentsOfSoldCollection:
-            db.collection(collection8).document(randomID).collection(usersCollection2).document(doc.id).collection(collectionOfUsersCollection2).document(docu11.id).set(docu11.to_dict())
-
-    customers_ref=db.collection(collection8).document(randomID).collection(usersCollection3)
+            db1.collection(collection8).document(toID).collection(usersCollection2).document(doc.id).collection(collectionOfUsersCollection2).document(docu11.id).set(docu11.to_dict())
+# transfer the customer collection data if transferCustomer value is true
+if(transferCustomer):
+    customers_ref=db1.collection(collection8).document(fromID).collection(usersCollection3)
     DocumentsOfCustomersCollection=customers_ref.get()
     for doc in DocumentsOfCustomersCollection:
-        db1.collection(collection8).document(randomID).collection(usersCollection3).document(doc.id).set(doc.to_dict())
-        
-        DocumentsOfPaymentCollection=db.collection(collection8).document(randomID).collection(usersCollection3).document(doc.id).collection(collectionsOfUsersCollection3[0]).get()
-        for docu in DocumentsOfPaymentCollection:
-            db1.collection(collection8).document(randomID).collection(usersCollection3).document(doc.id).collection(collectionsOfUsersCollection3[0]).document(docu.id).set(docu.to_dict())
-
-        DocumentsOfPaymentCollection1=db.collection(collection8).document(randomID).collection(usersCollection3).document(doc.id).collection(collectionsOfUsersCollection3[1]).get()
-        for docu in DocumentsOfPaymentCollection1:
-            db1.collection(collection8).document(randomID).collection(usersCollection3).document(doc.id).collection(collectionsOfUsersCollection3[1]).document(docu.id).set(docu.to_dict())
-
-        DocumentsOfPaymentCollection2=db.collection(collection8).document(randomID).collection(usersCollection3).document(doc.id).collection(collectionsOfUsersCollection3[2]).get()
-        for docu in DocumentsOfPaymentCollection2:
-            db1.collection(collection8).document(randomID).collection(usersCollection3).document(doc.id).collection(collectionsOfUsersCollection3[2]).document(docu.id).set(docu.to_dict())
-
-        
-
-    DocumentsOfSpeechInventoryCollection=db.collection(collection8).document(randomID).collection(usersCollection4).get()
+        db1.collection(collection8).document(toID).collection(usersCollection3).document(doc.id).set(doc.to_dict())
+        for c in collectionsOfUsersCollection3:
+            DocumentsOfPaymentCollection=db1.collection(collection8).document(fromID).collection(usersCollection3).document(doc.id).collection(c).get()
+            for docu in DocumentsOfPaymentCollection:
+                db1.collection(collection8).document(toID).collection(usersCollection3).document(doc.id).collection(c).document(docu.id).set(docu.to_dict())
+# transfer the speech collection data if transferSpeech value is true
+if(transferSpeech):
+    DocumentsOfSpeechInventoryCollection=db1.collection(collection8).document(fromID).collection(usersCollection4).get()
     for doc in DocumentsOfSpeechInventoryCollection:
-        db1.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).set(doc.to_dict())
-        
-        speechInventoryPriceDocuments=db.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).get()
-        
+        db1.collection(collection8).document(toID).collection(usersCollection4).document(doc.id).set(doc.to_dict())
+        speechInventoryPriceDocuments=db1.collection(collection8).document(fromID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).get()
         for docuDash in speechInventoryPriceDocuments:
-            db1.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).set(docuDash.to_dict())
-            RecordsDocuments=db.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).get()
+            db1.collection(collection8).document(toID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).set(docuDash.to_dict())
+            RecordsDocuments=db1.collection(collection8).document(fromID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).get()
             for docuDashDash in RecordsDocuments:
-                db1.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).set(docuDashDash.to_dict())
-                PriceDocuments=db.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).collection(collectionOfRecords[0]).get()
-                for docuDashDashDash in PriceDocuments:
-                    db1.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).collection(collectionOfRecords[0]).document(docuDashDashDash.id).set(docuDashDashDash.to_dict())
-                    
-                StockDocuments=db.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).collection(collectionOfRecords[1]).get()
-                for docuDashDashDash2 in StockDocuments:
-                    db1.collection(collection8).document(randomID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).collection(collectionOfRecords[0]).document(docuDashDashDash2.id).set(docuDashDashDash2.to_dict())
-                
-elapsed_time_secs=time.time()-start_time
-message="Execution took: %s secs" % timedelta(seconds=round(elapsed_time_secs))
-print(message)                
-        
-    
+                db1.collection(collection8).document(toID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).set(docuDashDash.to_dict())
+                for c in collectionOfRecords:
+                    PriceDocuments=db1.collection(collection8).document(fromID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).collection(c).get()
+                    for docuDashDashDash in PriceDocuments:
+                        db1.collection(collection8).document(toID).collection(usersCollection4).document(doc.id).collection(collectionsOfUsersCollection4).document(docuDash.id).collection(collectionOfSpeechInventoryPrice).document(docuDashDash.id).collection(c).document(docuDashDashDash.id).set(docuDashDashDash.to_dict())
+
+
