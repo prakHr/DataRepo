8df88e6c56d44c7e1d3d5d4e02f78ee5334a0b1f
zorro-p <f20150456@hyderabad.bits-pilani.ac.in>
2018-10-25 11:37:54 +0530

    updated Timestamp

--- Extractor360.py
+++ Extractor360.py
@@ -16,6 +16,21 @@ db1=extractDatabase(cred1)
 
 collection1=u'barcode_inventory'
 ref1=db1.collection(collection1)
+
+#update timestamp
+def updateTimestamp(collection,reference):
+    docs=reference.get()
+    for doc in docs:
+        RandomId,my_dict=doc.id,doc.to_dict()
+        if 'timestamp' not in my_dict:
+            field_updates={
+                'timestamp':1514745000000,
+            }
+            db1.collection(collection).document(RandomId).update(field_updates)
+
+updateTimestamp(collection1,ref1)
+
+'''
 #remove spaces from barcode_inventory
 def removeSpaces(collection,reference):
     docs=reference.get()
@@ -47,8 +62,16 @@ def totalLenInBarcodeInventory(reference,digit):
     return barcodes
 len14,len13=totalLenInBarcodeInventory(ref1,14),totalLenInBarcodeInventory(ref1,13)
 print(len(len14),len(len13))#8 259
-for b in len14:print(b)
-print('---------|------------|------')
-for b in len13:print(b)
+#for b in len14:print(b)
+#print('---------|------------|------')
+#for b in len13:print(b)
  
-def substringDelete():
+def substringDelete(array1,array2):
+    for len14 in array1:
+        for len13 in array2:
+            if len14[1:]==len13:
+                db1.collection(collection1).document(len14).delete()
+            if len14[:-1]==len13:
+                db1.collection(collection1).document(len14).delete()
+    
+'''
