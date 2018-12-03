26f5c548f04a00ee6ecc1a8eb6fdf7b260bbda2b
zorro-p <f20150456@hyderabad.bits-pilani.ac.in>
2018-12-03 13:05:50 +0530

    Added comments for readability

--- printer.py
+++ printer.py
@@ -1,3 +1,9 @@
+#######################################################################################
+# x,y= 2 strings ######################################################################
+# pageNumber = integer input which gives x+y+"00"+str(page)=> (1,length(pageNumber)) ##
+# create images using png format and then resized images ##############################
+# then saves into folders using corresponding names of x+y+"00"+str(page) #############
+#######################################################################################
 import barcode
 import img2pdf
 from barcode.writer import ImageWriter
@@ -15,7 +21,7 @@ def makeFolder(folderAddress,directoryName):
         os.mkdir(folderAddress+directoryName)
     except FileExistsError:
         pass
-        
+
 Code_128="code128"
 
 letter_words_1=input()
@@ -24,40 +30,33 @@ letter_words_2=input()
 def putIntoDocumentFiles(folderAddress,pic):
     document=docx.Document()
     table=document.add_table(rows=10,cols=4)
-    
+
     for row in table.rows:
         for cell in row.cells:
             paragraph=cell.paragraphs[0]
             run=paragraph.add_run()
-            run.add_picture(folderAddress+pic+".png",width=350000*1.42,height=700000*0.49)
+            run.add_picture(pic+".png",width=350000*1.42,height=700000*0.49)
 
-    for i in range(5):#0+4+1+5+1+10+1
-        document.save(folderAddress+"/"+pic+"/"+pic+"_"+str(i+1)+".docx")
-    
+    for i in range(100):
+        document.save(folderAddress+"docxDocuments"+pic[22:]+"_"+str(i+1)+".docx")
+
 
 def fd(image,savedName):
     width,height=image.size
     image=image.resize(int(width/2),int(height/2))
     image.save(savedName)
-    
+
 pageNumber=int(input())
-array,array2=[],[]
+array=[]
 for i in range(1,1+pageNumber):
     barcode_Name=letter_words_1+letter_words_2+"00"+str(i)
     ean=barcode.get(Code_128,barcode_Name,writer=ImageWriter())
-    filename=ean.save(folderAddress+barcode_Name)#saves to [MG001.png,MG002.png...MG00(pageNumber).png]
+    filename=ean.save(barcode_Name)
     array.append(folderAddress+barcode_Name+".png")
-    array2.append(barcode_Name)
-    
-
-time.sleep(2)
-
-for a,b in zip(array,array2):
-    #img=Image.open(a)#opens the image
-    #fd(img,a)#resized the image and saved the image
-    makeFolder(folderAddress,b)#a='/home/zorro/blockchain/'+''=>a[23:-4]+'.png'
-    time.sleep(0.2)#take a break
-    #putIntoDocumentFiles(folderAddress,a[23:-4])
-    putIntoDocumentFiles(folderAddress,b)
 
 
+time.sleep(3)
+
+for a in array:
+    makeFolder(folderAddress,a[23:-4])
+    putIntoDocumentFiles(folderAddress,a[:-4])
