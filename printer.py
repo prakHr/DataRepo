0642f1a13cbf74e8039d990d3dd68b3e6498f6a9
zorro-p <f20150456@hyderabad.bits-pilani.ac.in>
2018-12-03 16:06:51 +0530

    added faster solution

--- printer.py
+++ printer.py
@@ -1,9 +1,8 @@
-#######################################################################################
-# x,y= 2 strings ######################################################################
-# pageNumber = integer input which gives x+y+"00"+str(page)=> (1,length(pageNumber)) ##
-# create images using png format and then resized images ##############################
-# then saves into folders using corresponding names of x+y+"00"+str(page) #############
-#######################################################################################
+#x,y=input 2 letter words
+#adjust width and height to store 100 barcodes in a single Page 
+#Rows = 2500 for 100 pages
+#pageNumber = str(x)+str(y)+"00"+str(i)+".png" for i in range(input())
+
 import barcode
 import img2pdf
 from barcode.writer import ImageWriter
@@ -13,7 +12,9 @@ from PIL import Image
 import docx
 from docx.api import Document
 import os
+from time import time
 
+start_time=time()
 folderAddress="/home/zorro/blockchain/"
 
 def makeFolder(folderAddress,directoryName):
@@ -21,42 +22,54 @@ def makeFolder(folderAddress,directoryName):
         os.mkdir(folderAddress+directoryName)
     except FileExistsError:
         pass
-
+        
 Code_128="code128"
 
 letter_words_1=input()
 letter_words_2=input()
 
+def fasterSolution(folderAddress,pic):
+    document = Document()
+    COLUMNS=4
+    table=document.add_table(rows=1000,columns=COLUMNS)
+    table_cells=table._cells
+    for i in range(ROWS):
+        row_cells=table_cells[i*COLUMNS:(i+1)*COLUMNS]
+        for cell in row_cells.cells:
+            paragraph=cell.paragraphs[0]
+            run=paragraph.add_run()
+            run.add_picture(folderAddress+pic+".png",width=350000*1.42,height=700000*0.49)
+        
 def putIntoDocumentFiles(folderAddress,pic):
-    document=docx.Document()
-    table=document.add_table(rows=10,cols=4)
-
+    document = Document()
+    table=document.add_table(rows=25,cols=4)#ROWS=25 For 100 barcodes 
     for row in table.rows:
         for cell in row.cells:
             paragraph=cell.paragraphs[0]
             run=paragraph.add_run()
-            run.add_picture(pic+".png",width=350000*1.42,height=700000*0.49)
+            run.add_picture(folderAddress+pic+".png",width=350000*0.71,height=350000*0.49)#(width,height)=>dimensions(singleTableRow,singleTableColumn) for singlePage
 
-    for i in range(100):
-        document.save(folderAddress+"docxDocuments"+pic[22:]+"_"+str(i+1)+".docx")
-
-
-def fd(image,savedName):
-    width,height=image.size
-    image=image.resize(int(width/2),int(height/2))
-    image.save(savedName)
+    for i in range(2):
+        document.save(folderAddress+"singleFolder"+"/"+pic+"_"+str(i+1)+".docx")
+    
 
 pageNumber=int(input())
-array=[]
+array,array2=[],[]
 for i in range(1,1+pageNumber):
     barcode_Name=letter_words_1+letter_words_2+"00"+str(i)
     ean=barcode.get(Code_128,barcode_Name,writer=ImageWriter())
-    filename=ean.save(barcode_Name)
+    filename=ean.save(folderAddress+barcode_Name)#saves to [MG001.png,MG002.png...MG00(pageNumber).png]
     array.append(folderAddress+barcode_Name+".png")
+    array2.append(barcode_Name)
+    
+#time.sleep(2)
 
-
-time.sleep(3)
-
-for a in array:
-    makeFolder(folderAddress,a[23:-4])
-    putIntoDocumentFiles(folderAddress,a[:-4])
+makeFolder(folderAddress,"singleFolder1")
+for a,b in zip(array,array2):
+    #time.sleep(0.2)
+    putIntoDocumentFiles(folderAddress,b)
+    
+end_time=time()
+time_taken = end_time - start_time # time_taken is in seconds
+hours, rest = divmod(time_taken,3600)
+minutes, seconds = divmod(rest, 60)
